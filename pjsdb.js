0e8b3a3343f3da09ec288611b31d7b6faa3a4b8f
juliencalcada <133039635+juliencalcada@users.noreply.github.com>
2023-05-28 21:01:51 +0200

    Added getElement function and comments
    Added the getElement function to the JavaScript code, along with detailed comments explaining its purpose, usage, and request flow. The function allows retrieving elements from the server-side PHP script by sending an AJAX request and handling the response asynchronously using Promises. The comments provide clarity on the function's parameters, XML file retrieval, XPath query construction, and the conversion of the result to JSON format. This commit focuses on extending the JavaScript functionality by introducing the getElements function, making it easier to fetch specific elements from the server-side database.

--- pjsdb.js
+++ pjsdb.js
@@ -1,64 +1,135 @@
-// Fonction pour ajouter un élément
+// Function to add an element
 function addElement(filename, name, value) {
+    // Create an XMLHttpRequest object
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "pjsdb.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
+        // Check if the request has been completed
         if (xhr.readyState === XMLHttpRequest.DONE) {
+            // Check the response status
             if (xhr.status === 200) {
-                console.log("Élément ajouté avec succès !");
+                console.log("Element added successfully!");
             } else {
-                console.error("Erreur lors de l'ajout de l'élément : " + xhr.status);
+                console.error("Error adding element: " + xhr.status);
             }
         }
     };
+    // Send the request to the server with the necessary parameters
     xhr.send("action=addElement&filename=" + encodeURIComponent(filename) + "&name=" + encodeURIComponent(name) + "&value=" + encodeURIComponent(value));
 }
 
-// Fonction pour mettre à jour un élément
+// Function to update an element
 function updateElement(filename, name, newValue, identifier) {
+    // Create an XMLHttpRequest object
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "pjsdb.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
+        // Check if the request has been completed
         if (xhr.readyState === XMLHttpRequest.DONE) {
+            // Check the response status
             if (xhr.status === 200) {
-                console.log("Élément mis à jour avec succès !");
+                console.log("Element updated successfully!");
             } else {
-                console.error("Erreur lors de la mise à jour de l'élément : " + xhr.status);
+                console.error("Error updating element: " + xhr.status);
             }
         }
     };
+    // Send the request to the server with the necessary parameters
     xhr.send("action=updateElement&filename=" + encodeURIComponent(filename) + "&name=" + encodeURIComponent(name) + "&newValue=" + encodeURIComponent(newValue) + "&identifier=" + encodeURIComponent(identifier));
 }
 
-// Fonction pour supprimer un élément
+// Function to delete an element
 function deleteElement(filename, name, identifier = null) {
+    // Create an XMLHttpRequest object
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "pjsdb.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = function() {
+        // Check if the request has been completed
         if (xhr.readyState === XMLHttpRequest.DONE) {
+            // Check the response status
             if (xhr.status === 200) {
-                console.log("Élément supprimé avec succès !");
+                console.log("Element deleted successfully!");
             } else {
-                console.error("Erreur lors de la suppression de l'élément : " + xhr.status);
+                console.error("Error deleting element: " + xhr.status);
             }
         }
     };
+    // Send the request to the server with the necessary parameters
     xhr.send("action=deleteElement&filename=" + encodeURIComponent(filename) + "&name=" + encodeURIComponent(name) + "&identifier=" + encodeURIComponent(identifier));
 }
+
+// Function to get elements
+function getElements(filename, name = null, identifier = null) {
+    // Return a promise to handle asynchronous operations
+    return new Promise(function(resolve, reject) {
+        // Create an XMLHttpRequest object
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "pjsdb.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            // Check if the request has been completed
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                // Check the response status
+                if (xhr.status === 200) {
+                    // Parse the JSON response into an array of elements
+                    var elements = JSON.parse(xhr.responseText);
+                    // Resolve the promise with the retrieved elements
+                    resolve(elements);
+                } else {
+                    // Reject the promise with an error message
+                    reject("Error retrieving elements: " + xhr.status);
+                }
+            }
+        };
+
+        // Send the request to the server with the necessary parameters based on the provided arguments
+        if (name !== null && identifier !== null) {
+            xhr.send("action=getElement&filename=" + encodeURIComponent(filename) + "&name=" + encodeURIComponent(name) + "&identifier=" + encodeURIComponent(identifier));
+        } else if (name !== null) {
+            xhr.send("action=getElement&filename=" + encodeURIComponent(filename) + "&name=" + encodeURIComponent(name));
+        } else {
+            xhr.send("action=getElement&filename=" + encodeURIComponent(filename));
+        }
+    });
+}
+
+
+
 
 // Usage examples
 
 // Add a new element with an automatically incremented identifier
-addElement("filename2", "name", "John Doe");
+addElement("filename", "name", "John Doe");
 
 // Update the value of an element with a specified identifier
-updateElement("filename2", "name", "Jane Smith", 1);
+updateElement("filename", "name", "Jane Smith", 1);
 
-// // Delete all elements with a specified name
-deleteElement("filename2", "name");
+// Delete all elements with a specified name
+deleteElement("filename", "name");
 
 // Delete a single element with a specified identifier
-deleteElement("filename", "name", 1);
+deleteElement("filename", "name", 2);
+
+// Get all elements
+getElements("filename").then(function(elements) {
+    console.log(elements);
+}).catch(function(error) {
+    console.error(error);
+});
+
+// Get all elements with a specified name
+getElements("filename", "name").then(function(elements) {
+    console.log(elements);
+}).catch(function(error) {
+    console.error(error);
+});
+
+// Get a single element with a specified name and identifier
+getElements("filename", "name", 1).then(function(elements) {
+    console.log(elements);
+}).catch(function(error) {
+    console.error(error);
+});
